2bf37d017c08f8a1669f4c6c6eba3ecc8310b186
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-08-10 14:41:50 -0600

    cards: update image name.

--- sushi_go/lib/models/sushi_go_card.dart
+++ sushi_go/lib/models/sushi_go_card.dart
@@ -2,6 +2,12 @@ class SushiGoCard {
   final int id;
   final int uid;
   final String name;
+  final String img;
 
-  SushiGoCard({this.id, this.uid, this.name});
+  SushiGoCard({
+    this.id,
+    this.uid,
+    this.name,
+    this.img,
+  });
 }
